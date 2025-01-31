d74397bd08e9345eb18e37b8d33d3f1163e85e6b
AnnonymousReview <32741227+AnnonymousForPapers@users.noreply.github.com>
2025-01-31 13:50:20 -0500

    Add files via upload

---
+++ ALFWorld/ALFWorld_test/Adaptation/SALA/RFLX_append_NoExplitcit_rmvRflx1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --partition=general-gpu               # Name of Partition
+#SBATCH --gres=gpu:1                          # Request 2 GPU cards for the job
+#SBATCH --constraint=epyc64
+#SBATCH --constraint=a100
+#SBATCH --output=R-%x_%j.out
+
+module purge
+
+source /gpfs/homefs1/ych22001/miniconda3/etc/profile.d/conda.sh
+
+conda activate HF_LLM
+
+python Append_RFLX_noExplicit_rmvRflx1.py
